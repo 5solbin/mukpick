cd3974ae85855cfa5d26954a828c5cb022744bee
5solbin <dhrhfqls@naver.com>
2026-03-19 16:23:26 +0900

    first commit

---
+++ src/main/java/solsolsol/mukpick/MukpickApplication.java
@@ -0,0 +1,13 @@
+package solsolsol.mukpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MukpickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MukpickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/solsolsol/mukpick/MukpickApplicationTests.java
@@ -0,0 +1,13 @@
+package solsolsol.mukpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MukpickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
